8701645dc540468479059dbd59e54e6fbbb9f172
NS00500048 <NS00500048@inbahrpc02609.TechMahindra.com>
2019-02-08 19:14:45 +0530

    Added Object Repositories

---
+++ Object Repository/Dealers Info.rs
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description>All Dealers in Locate Cache</description>
+   <name>Dealers Info</name>
+   <tag></tag>
+   <elementGuidId>a7f73597-9400-47c2-8878-11d95f262573</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <httpBody></httpBody>
+   <httpBodyContent>{
+  &quot;text&quot;: &quot;\u003csearchrequest mode\u003d\&quot;dealer\&quot;\u003e\n    \u003csourceapplication\u003eDealerService\u003c/sourceapplication\u003e\n    \u003cbtovisitordescriptor\u003e1\u003c/btovisitordescriptor\u003e\n    \u003cconfigurationid\u003e1\u003c/configurationid\u003e\n    \u003ccriteria\u003e\n    \u003c/criteria\u003e\n    \u003cselect records\u003d\&quot;10000\&quot;\u003e\n        \u003cfields\u003e\n            \u003c!--\u003cfield name\u003d\&quot;*\&quot;/\u003e--\u003e\n            \u003cfield name\u003d\&quot;dealerPAcode\&quot;/\u003e\n            \u003cfield name\u003d\&quot;name\&quot;/\u003e\n            \u003cfield name\u003d\&quot;FordImagePreference\&quot;/\u003e\n            \u003cfield name\u003d\&quot;LincolnImagePreference\&quot;/\u003e\n            \u003cfield name\u003d\&quot;zip\&quot;/\u003e\n        \u003c/fields\u003e\n    \u003c/select\u003e\n\u003c/searchrequest\u003e&quot;,
+  &quot;contentType&quot;: &quot;text/plain&quot;,
+  &quot;charset&quot;: &quot;UTF-8&quot;
+}</httpBodyContent>
+   <httpBodyType>text</httpBodyType>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Content-Type</name>
+      <type>Main</type>
+      <value>text/plain</value>
+   </httpHeaderProperties>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>POST</restRequestMethod>
+   <restUrl>http://40.85.167.114:3005/locate-proxy?</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceFunction></soapServiceFunction>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
---
+++ Object Repository/Prod Request.rs
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>Prod Request</name>
+   <tag></tag>
+   <elementGuidId>59ed8a9f-ceec-4660-8aef-e414ae2eb168</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <httpBody></httpBody>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>ocp-apim-subscription-key</name>
+      <type>Main</type>
+      <value>c99182f104f645499a5bc9bf779f8ec7</value>
+   </httpHeaderProperties>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>GET</restRequestMethod>
+   <restUrl>https://dealerservices.azure-api.net/services/inventory/1FADP3E2XJL320893/images/1?</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceFunction></soapServiceFunction>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
